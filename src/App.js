e3f849da8299f0117c71ade19dd4d88c6314a660
Anis Boutouria <enisboutouria5@gmail.com>
2021-07-15 00:51:56 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import react from "react";
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <form>
+          <p>Create Account</p>
+          <input type={Text}></input>
+          <p>Enter Your Email</p>
+          <input type={Text}></input>
+          <p>Enter Your password</p>
+          <input type={Text} ></input>
+          <br>
+          </br>
+          <br>
+          </br>
+          <button color="primary" typt="submit">
+            Submit
+          </button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
